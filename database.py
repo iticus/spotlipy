cf3749607e8298830c586b59e17a4d4b526ee70c
iticus <ticus.ionut@gmail.com>
2016-07-05 23:53:36 +0300

    implemented database functions, improved logging

--- database.py
+++ database.py
@@ -4,20 +4,63 @@ Created on Jun 21, 2016
 @author: ionut
 '''
 
+import datetime
+import logging
 import psycopg2
+from psycopg2.extras import DictCursor
 
 import settings
 
+logger = logging.getLogger('database')
 _dbconn = psycopg2.connect(settings.DSN)
 
 
-def insert_song(channel, artist, title, moment):
-    cursor = _dbconn.cursor()
-    query = "INSERT into songs(channel, artist, title, moment) VALUES(%s, %s, %s, %s)"
-    cursor.execute(query, (channel, artist, title, moment))
+def insert_song(song):
+    cursor = _dbconn.cursor(cursor_factory=DictCursor)
+    query = "INSERT into songs(channel, artist, title, played, found) VALUES(%s, %s, %s, %s, %s) RETURNING id"
+    try:
+        cursor.execute(query, (song['channel'], song['artist'], song['title'], song['played'], datetime.datetime.now()))
+        _dbconn.commit()
+    except psycopg2.IntegrityError:
+        logger.warning('could not insert song %s as it is already present' % song['title'])
+        _dbconn.rollback()
+        return False
+        
+    except Exception as e:
+        logger.error('could not insert song %s: %s' % (song['title'], e))
+        _dbconn.rollback()
+        return False
+        
     rows = cursor.fetchall()
+    cursor.close()
     return len(rows) > 0
 
 
-def get_songs():
-    pass
+def get_unprocessed_songs():
+    cursor = _dbconn.cursor(cursor_factory=DictCursor)
+    query = "SELECT id, channel, artist, title FROM songs WHERE status=0"
+    try:
+        cursor.execute(query)
+    except Exception as e:
+        logger.error('could not retrieve unprocessed songs: %s' % e)
+        return []
+    
+    rows = cursor.fetchall()
+    cursor.close()
+    return rows
+
+
+def update_song(song_id, status, spotify_id=None):
+    cursor = _dbconn.cursor(cursor_factory=DictCursor)
+    query = "UPDATE songs SET status=%s, spotify_id=%s WHERE id=%s RETURNING id"
+    try:
+        cursor.execute(query, (song_id, spotify_id, status))
+        _dbconn.commit()
+    except Exception as e:
+        logger.error('could not update song %s: %s' % (song_id, e))
+        _dbconn.rollback()
+        return False
+        
+    rows = cursor.fetchall()
+    cursor.close()
+    return len(rows) > 0    
--- database.sql
+++ database.sql
@@ -1,13 +1,30 @@
+--Postgres database structure for spotlipy
+
 --songs
 CREATE TABLE songs(
 	id bigserial PRIMARY KEY,
-    channel text,
-    artist text,
-    title text,
-    played timestamp without time zone,
-    found timestamp without time zone
+    channel text NOT NULL,
+    artist text NOT NULL,
+    title text NOT NULL,
+    played timestamp without time zone NOT NULL,
+    found timestamp without time zone NOT NULL,
+    status smallint NOT NULL DEFAULT 0,
+    spotify_id text DEFAULT NULL,
+    UNIQUE (channel, artist, title)
 );
-
 CREATE INDEX ON songs(channel);
 CREATE INDEX ON songs(artist);
 CREATE INDEX ON songs(played);
+CREATE INDEX ON songs(status);
+--status: 0 unprocessed, 1 - found on Spotify, 2 - not found on Spotify
+
+
+--playlists
+CREATE TABLE playlists(
+	id bigserial PRIMARY KEY,
+    channel text NOT NULL,
+    spotify_playlist text NOT NULL,
+    added timestamp without time zone NOT NULL
+);
+CREATE INDEX ON playlists(channel);
+CREATE INDEX ON playlists(spotify_playlist);
